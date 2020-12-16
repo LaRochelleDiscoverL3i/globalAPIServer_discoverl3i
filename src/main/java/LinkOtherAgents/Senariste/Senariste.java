7597e18ea1586492b962dc2e0ca90d7d83855b8d
Justin Metayer <metayerjl@live.fr>
2020-12-16 17:18:32 +0100

    end link with agents

--- src/main/java/LinkOtherAgents/Senariste/Senariste.java
+++ src/main/java/LinkOtherAgents/Senariste/Senariste.java
@@ -56,7 +56,34 @@ public class Senariste {
 
     public static JsonObject senariste_post_curiosite(JsonObject content){
         try{
-            return null;
+            FileInputStream fis = new FileInputStream("src/main/java/config/server_config.properties");
+            agents_config.load(fis);
+
+            String url_observer = agents_config.getProperty("senariste_url");
+            int port_observer = Integer.valueOf(agents_config.getProperty("senariste_port"));
+            String requestUrl = "/curiosite";
+
+            final Vertx vertx = Vertx.vertx();
+            WebClient client = WebClient.create(vertx);
+
+            AtomicReference<JsonObject> result = new AtomicReference<>(new JsonObject());
+
+            client.post(port_observer, url_observer, requestUrl).sendJsonObject(content).onSuccess(
+                    response -> {
+                        LOGGER.info("[Analyste] Method : anayliste_post_creation - onSuccess return");
+                        result.set(response.bodyAsJsonObject());
+                    }
+            ).onFailure(
+                    error -> {
+                        LOGGER.warn("[Analyste] Method : anayliste_post_creation - Error message : "+error.getMessage());
+
+                        final JsonObject jsonResponse = new JsonObject();
+                        jsonResponse.put("Error", error.getMessage());
+                        result.set(jsonResponse);
+                    }
+            );
+
+            return result.get();
         }catch (Exception e){
             LOGGER.warn("[Analyste] Method : senariste_post_curiosite - Error message : "+e.getMessage());
             return null;
@@ -65,7 +92,34 @@ public class Senariste {
 
     public static JsonObject senariste_post_pogression(JsonObject content){
         try{
-            return null;
+            FileInputStream fis = new FileInputStream("src/main/java/config/server_config.properties");
+            agents_config.load(fis);
+
+            String url_observer = agents_config.getProperty("senariste_url");
+            int port_observer = Integer.valueOf(agents_config.getProperty("senariste_port"));
+            String requestUrl = "/progression";
+
+            final Vertx vertx = Vertx.vertx();
+            WebClient client = WebClient.create(vertx);
+
+            AtomicReference<JsonObject> result = new AtomicReference<>(new JsonObject());
+
+            client.post(port_observer, url_observer, requestUrl).sendJsonObject(content).onSuccess(
+                    response -> {
+                        LOGGER.info("[Analyste] Method : anayliste_post_creation - onSuccess return");
+                        result.set(response.bodyAsJsonObject());
+                    }
+            ).onFailure(
+                    error -> {
+                        LOGGER.warn("[Analyste] Method : anayliste_post_creation - Error message : "+error.getMessage());
+
+                        final JsonObject jsonResponse = new JsonObject();
+                        jsonResponse.put("Error", error.getMessage());
+                        result.set(jsonResponse);
+                    }
+            );
+
+            return result.get();
         }catch (Exception e){
             LOGGER.warn("[Analyste] Method : senariste_post_pogression - Error message : "+e.getMessage());
             return null;
